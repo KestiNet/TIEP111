7c435be6d675f9cd147299edc976090c8d296d5c
KestiNet <esakesti@gmail.com>
2022-10-11 20:01:24 +0100

    Update Tarkkuus.java

--- src/demo/dd4/Tarkkuus.java
+++ src/demo/dd4/Tarkkuus.java
@@ -6,6 +6,27 @@ package demo.dd4;
  *
  */
 public class Tarkkuus {
+    
+    
+    
+    /**
+     * @param x verrattava luku
+     * @param eps virheraja, eps aina >= 0
+     * @return 1 tai -1 riippuen kumpi on suurempi
+     *  @example
+     * <pre name="test">
+     * etumerkki(0,0)  ~~~ 0;
+     * etumerkki(1,0)  ~~~ 1;
+     * etumerkki(0.4,0.3)  ~~~ 1;
+     * etumerkki(-1,0) ~~~ -1;
+     * etumerkki(0.5, 0.2) ~~~ 1;
+     * </pre>
+     */
+    public static int etumerkki(double x, double eps) {
+        if (x < -eps) return -1;
+        if (x >  eps) return 1;
+        return 0;
+    }
 
     /**
      * @param args ei käytössä
@@ -19,27 +40,6 @@ public class Tarkkuus {
 
     }
     
-    
-    /**
-     * @param x verrattava luku
-     * @param eps virheraja, eps aina >= 0
-     * @return -1, jos x < -eps \n
-     *          0, jos -eps <= x <= eps \n
-     *          1, jos x > eps
-     *  @example
-     * <pre name="test">
-     * etumerkki(0,0)  ~~~ 0;
-     * etumerkki(1,0)  ~~~ 1;
-     * etumerkki(2,0)  ~~~ 1;
-
-     * etumerkki(-1,0) ~~~ -1;
-     * etumerkki(-0.51, 0.5) ~~~ -1;
-     * </pre>
-     */
-    public static int etumerkki(double x, double eps) {
-        if (x < -eps) return -1;
-        if (x >  eps) return 1;
-        return 0;
-    }
+ 
 
 }
