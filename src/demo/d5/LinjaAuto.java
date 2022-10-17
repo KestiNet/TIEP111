f4144033c860202396c7765346d9bac7c7724764
KestiNet <esakesti@gmail.com>
2022-10-17 19:09:39 +0100

    Update LinjaAuto.java
    :(

--- src/demo/d5/LinjaAuto.java
+++ src/demo/d5/LinjaAuto.java
@@ -4,14 +4,14 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 
 /**
- * @author lange
+ * @author Esa Kesti
  * @version 17.10.2022
  *
  */
 public class LinjaAuto {
     
     private int paikat = 60;
-    private int vapaitaPaikkoja = 10;
+    private int vapaitaPaikkoja;
     
     /**
      * Muodostaja LinjaAuto luokalle
@@ -25,41 +25,95 @@ public class LinjaAuto {
     }
     
     /**
-     * Tulostus metodi
-     * @param os outputstream
+     * muodostaja
+     * @param paikat paikkojen maara
      */
-    public void tulosta(OutputStream os) {
-        PrintStream out = new PrintStream(os);
-        out.println();
-    }
-    
-    public void lisaa(int lisaaMatkustajia) {
-        
-    }
-    
-    public void vahenna(int vahennaMatkustajia) {
-        
+    public LinjaAuto(int paikat) {
+        this.paikat = paikat;
+        //this.vapaitaPaikkoja = vapaitaPaikkoja;
     }
 
     /**
      * @return paikat paikkojen maaran
      */
-    public int getPaikat() {
-        return paikat;
-    }
+    //public int getPaikat() {
+      //  return paikat;
+    //}
     
     /**
      * @return vapaiden paikkojen maaaran
      */
-    public int getvapaitaPaikkoja() {
+    public int getTilaa() {
         return vapaitaPaikkoja;
+        
+       }
+    
+    
+    
+    /**
+     * Tulostus metodi
+     * @param os outputstream
+     */
+    public void tulosta(OutputStream os) {
+        PrintStream out = new PrintStream(os);
+        int matkustajat = paikat-vapaitaPaikkoja;
+        out.println("Linja-autoon mahtuu" + paikat + "matkustajia talla hetkella: " + matkustajat + "vapaita paikkoja jaljella: " + vapaitaPaikkoja);
     }
+    
+    /**
+     * @param lisaaMatkustajia lisätään matkustajia
+     * @return palauttaa montako matkustajaa on liikaa tai 0 jos ehto ei toteudu
+     */
+    public int lisaa(int lisaaMatkustajia) {
+       if (lisaaMatkustajia > vapaitaPaikkoja) {
+           int liikaaMatkustajia = vapaitaPaikkoja - lisaaMatkustajia;
+           return liikaaMatkustajia;
+       }
+    vapaitaPaikkoja -= lisaaMatkustajia;
+       
+       return 0;
+    }
+    
+    /**
+     * @param vahennaMatkustajia vähennetään matkustajia
+     * @return palauttaa paljonko matkustajia poistui kyydista 
+     * paivittaa myos vapaiden paikkojen maaran
+     */
+    public int vahenna(int vahennaMatkustajia) {
+        int matkustajat = paikat - vahennaMatkustajia;
+        if ( matkustajat > paikat) {
+           return matkustajat - paikat;
+            
+        }
+        vapaitaPaikkoja += vahennaMatkustajia;
+        return 0;
+    }
+
+   
+    
+    
     /**
      * @param args ei käytössä
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        LinjaAuto pikkubussi = new LinjaAuto(10);
+        LinjaAuto isobussi = new LinjaAuto(45);
+        pikkubussi.lisaa(4);    pikkubussi.tulosta(System.out); // 10,4,6
+        isobussi.lisaa(30);     isobussi.tulosta(System.out);   // 45,30,15
+        int yli = pikkubussi.lisaa(15);
+        isobussi.lisaa(yli);
+        pikkubussi.tulosta(System.out);                         // 10,10,0
+        isobussi.tulosta(System.out);                           // 45,39,6
+        if ( pikkubussi.getTilaa() > 0 )
+            System.out.println("Pieneen bussiin mahtuu!");   // ei tulosta
+      //  if ( isobussi.getTilaa() )
+        //    System.out.println("Isoon bussiin mahtuu!");     // tulostaa
+        int vajaa = pikkubussi.vahenna(12);                  // vajaa = -2
+        if ( vajaa < 0 )
+            System.out.println("Pikkubussissa ei edes ole näin montaa!");
+        pikkubussi.tulosta(System.out);
     }
+
+  
 
 }
