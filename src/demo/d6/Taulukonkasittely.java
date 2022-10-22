26d9d78cdcf20bf1a4851d46142422e72938cf4a
KestiNet <esakesti@gmail.com>
2022-10-22 23:33:23 +0100

    Demo6 start

---
+++ src/demo/d6/Taulukonkasittely.java
@@ -0,0 +1,13 @@
+package demo.d6;
+
+import fi.jyu.mit.ohj2.*;
+
+/**
+ * @author Esa Kesti
+ * @version 22.10.2022
+ *
+ */
+public class Taulukonkasittely {
+
+    int kPituudet[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+}
