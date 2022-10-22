be0726b76715b3171f6706a557007420e321c429
KestiNet <esakesti@gmail.com>
2022-10-23 00:51:05 +0100

    Taulukon kasittely tehtava valmis

--- src/demo/d6/Taulukonkasittely.java
+++ src/demo/d6/Taulukonkasittely.java
@@ -8,6 +8,39 @@ import fi.jyu.mit.ohj2.*;
  *
  */
 public class Taulukonkasittely {
+    
+    /**
+     * Aliohjelma joka palauttaa taulukon pieninmmän alkion indeksin
+     * @param pienin taulukko
+     * @return palauttaa pienimmän alkion
+     * <pre name="test">   
+     * int t[] = {3,2,1,2,3};
+     * pienimmanPaikka(t) === 2;
+     * </pre>
+     * 
+     */
+    public static int pienimmanPaikka(int [] pienin) {
+     
+        int pieninAlkio = 0;
+        int pieni = pienin[pieninAlkio];
 
-    int kPituudet[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        for (int i = 1; i < pienin.length; i++){
+            if (pienin[i] <= pieni){
+            pieni = pienin[i];
+            pieninAlkio = i;
+            }
+        }
+        return pieninAlkio;
+    }
+
+    /**
+     * @param args ei käytössä
+     */
+    public static void main(String[] args) {
+        
+    int kPituudet[] = {31,7,28,31,30,31,30,31,31,30,31,30,31};
+    
+    System.out.println("Taulukon pienimmän alkion indeksi on: " + pienimmanPaikka(kPituudet));
+    }
+    
 }
