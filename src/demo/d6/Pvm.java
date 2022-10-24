2de7e1ecd070a0a01499d05ec385d67d69313344
KestiNet <esakesti@gmail.com>
2022-10-24 01:09:53 +0100

    Added compareTo funtion to Pvm class

--- src/demo/d6/Pvm.java
+++ src/demo/d6/Pvm.java
@@ -1,10 +1,263 @@
 package demo.d6;
 
+import fi.jyu.mit.ohj2.*;
+
+/**
+ * Alustava luokka päivämäärää varten
+ * @author Vesa Lappalainen
+ * @version 1.0, 07.02.2003
+ * @version 1.1, 14.02.2003
+ * @version 1.2, 17.02.2003
+ * @version 1.3, 11.02.2008
+ */
 public class Pvm {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    private int pv;
+    private int kk;
+    private int vv;
 
+
+    /**
+     *  Muuttaa päivämäärän nykypäivälle.
+     *  Todo: pitää vaihtaa hakemaan päivämäärä oikeasti. 
+     *  Mutta perinnässäkään ei nyt saa käyttää enempää attribuutteja kuin tässä on
+     */
+    public void paivays() {
+        pv = 13;
+        kk = 2;
+        vv = 2012;
     }
 
+
+    /**
+     * Alustetaan päivämäärä. 0-arvot eivät muuta vastaavaa attribuuttia  
+     * TODO: oikeellisuustarkistukset
+     * @param ipv päivän alustus
+     * @param ikk kuukauden alustus
+     * @param ivv vuoden alustus
+     */
+    public void alusta(int ipv, int ikk, int ivv) {
+        if ( ipv > 0 ) this.pv = ipv;
+        if ( ikk > 0 ) this.kk = ikk;
+        if ( ivv > 0 ) this.vv = ivv;
+        if ( this.vv < 50 ) this.vv += 2000;
+        if ( this.vv < 100 ) this.vv += 1900;
+    }
+
+
+    /** Alustetaan kaikki attribuutit oletusarvoon */
+    public Pvm() {
+        this(0, 0, 0);
+    }
+
+
+    /** 
+     * Alustetaan kuukausi ja vuosi oletusarvoon
+     * @param pv päivän alustusarvo
+     */
+    public Pvm(int pv) {
+        this(pv, 0, 0);
+    }
+
+
+    /**
+     * Alustetaan vuosi oletusarvoon
+     * @param pv päivän alustusarvo
+     * @param kk kuukauden oletusarvo
+     */
+    public Pvm(int pv, int kk) {
+        this(pv, kk, 0);
+    }
+
+
+    /**
+     * Alustetaan vuosi oletusarvoon
+     * @param pv päivän alustusarvo
+     * @param kk kuukauden oletusarvo
+     * @param vv vuoden alustusarvo
+     */
+    public Pvm(int pv, int kk, int vv) {
+        paivays();
+        alusta(pv, kk, vv);
+    } 
+
+
+    /**
+     * Alustetaan päivämäärä merkkijonosta
+     * @param s muotoa 12.3.2008 oleva merkkijono
+     */
+    public Pvm(String s) {
+        paivays();
+        pvmParse(s);
+    } 
+
+
+    /**
+     * Päivämäärä merkkijonona
+     * @return päivämäärä muodossa 17.2.2007
+     * @example
+     * <pre name="test">
+     *   Pvm tammi2011 = new Pvm(1,1,2011);
+     *   tammi2011.toString() === "1.1.2011"
+     *   Pvm helmi2011 = new Pvm(1,2,2011);
+     *   helmi2011.toString() === "1.2.2011"
+     *   Pvm tanaan = new Pvm(14,2,2011);
+     *   tanaan.toString()    === "14.2.2011"
+     *   Pvm maalis97 = new Pvm(1,3,1997);
+     *   maalis97.toString()  === "1.3.1997"
+     * </pre>
+     */
+    @Override
+    public String toString() {
+        return pv + "." + kk + "." + vv;
+    }
+
+
+    /**
+     * Ottaa päivämäärän tiedot merkkijonosta joka on muotoa 17.2.2007
+     * Jos joku osa puuttuu, sille käytetään tämän päivän arvoa oletuksena.
+     * @param sb tutkittava merkkijono
+     */
+    protected final void pvmParse(StringBuilder sb) {
+        int p = Mjonot.erota(sb, '.', 0);
+        int k = Mjonot.erota(sb, '.', 0);
+        int v = Mjonot.erota(sb, ' ', 0);
+        alusta(p, k, v);
+        // tai alusta(Mjonot.erota(sb,'.',0),Mjonot.erota(sb,'.',0),Mjonot.erota(sb,'.',0));
+    }
+
+
+    /**
+     * Ottaa päivämäärän tiedot merkkijonosta joka on muotoa 17.2.2007
+     * Jos joku osa puuttuu, sille käytetään tämän päivän arvoa oletuksena.
+     * @param s tutkittava merkkijono
+     */
+    protected final void pvmParse(String s) {
+        pvmParse(new StringBuilder(s));
+    }
+
+
+    /**
+     * Ottaa päivämäärän tiedot merkkijonosta joka on muotoa 17.2.2007
+     * Jos joku osa puuttuu, sille käytetään tämän päivän arvoa oletuksena.
+     * @param s tutkittava merkkijono
+     * 
+     * @example
+     * <pre name="test">
+     * Pvm pvm = new Pvm(11,3,2003);
+     * pvm.parse("12");           pvm.toString() === "12.3.2003";
+     * pvm.parse("..2001");       pvm.toString() === "12.3.2001";
+     * pvm.parse("..2009 14:30"); pvm.toString() === "12.3.2009"; 
+     * </pre>
+     */
+    public void parse(String s) {
+        pvmParse(s);
+    }
+
+
+    /**
+     * Ottaa päivämäärän tiedot merkkijonosta joka on muotoa 17.2.2007
+     * Jos joku osa puuttuu, sille käytetään tämän päivän arvoa oletuksena.
+     * Merkkijonosta otetaan pois vain se osa, jota tarvitaan.
+     * @param sb tutkittava merkkijono
+     * 
+     * @example
+     * <pre name="test">
+     * Pvm pvm = new Pvm(11,3,2003);
+     * StringBuilder jono = new StringBuilder("12");
+     * pvm.parse(jono); pvm.toString() === "12.3.2003"; jono.toString() === "";
+     * jono = new StringBuilder("..2001");
+     * pvm.parse(jono); pvm.toString() === "12.3.2001"; jono.toString() === "";
+     * jono = new StringBuilder("..2009 14:30");
+     * pvm.parse(jono); pvm.toString() === "12.3.2009"; jono.toString() === "14:30";
+     * </pre>
+     */
+    public void parse(StringBuilder sb) {
+        pvmParse(sb);
+    }
+
+
+    // Lisätty saantimetodit:
+
+    /**
+     * @return päivän arvo
+     * @example
+     * <pre name="test">
+     *   Pvm pv = new Pvm("14.2.2011");
+     *   pv.getPv() === 14;
+     * </pre>
+     */
+    public int getPv() {
+        return pv;
+    }
+
+
+    /**
+     * @return kuukauden arvo
+     * @example
+     * <pre name="test">
+     *   Pvm pv = new Pvm("14.2.2011");
+     *   pv.getKk() === 2;
+     * </pre>
+     */
+    public int getKk() {
+        return kk;
+    }
+
+
+    /**
+     * @return vuoden arvo
+     * @example
+     * <pre name="test">
+     *   Pvm pv = new Pvm("14.2.2011");
+     *   pv.getVv() === 2011;
+     * </pre>
+     */
+    public int getVv() {
+        return vv;
+    }
+    
+    /**
+     * funktio joka tarkistaa onko pv1 pienempi kuin pv2
+     * @param pv1 päivämäärä 1
+     * @param pv2 päivämäärä 2
+     * @return joko 0 jos sama tai -1 jos pv1 on pienempi kuin pv2
+     * <pre name="test">
+     * Pvm pv1 = new Pvm(21, 1, 2015);
+     * Pvm pv2 = new Pvm(1, 1, 1990);
+     * Pvm.compareTo(pv1, pv2) === 1;
+     * Pvm.compareTo(pv2, pv1) === -1;
+     *
+     * Pvm pv3 = new Pvm(1,1,1990);
+     * Pvm.compareTo(pv1, pv3) === 1;
+     * Pvm.compareTo(pv2, pv3) === 0;
+     * </pre>
+     */
+    public static int compareTo(int pv1, int pv2) {
+        if(pv1 == pv2) {
+            return 0;
+        }
+        if(pv1 - pv2 < 0) {
+            return -1;
+        }
+        return 1;
+        
+    }
+
+
+    /**
+     * Testataan päivämäärä-luokkaa
+     * @param args ei käytössä
+     */
+    public static void main(String[] args) {
+        Pvm pvm = new Pvm();
+
+        pvm.parse("12.1.1995");
+        System.out.println(pvm);
+        pvm.parse("15.3");
+        System.out.println(pvm);
+        pvm.parse("14");
+        System.out.println(pvm.getPv());
+
+    }
 }
